36493de38189e9db7824c4ee951c6805121343f8
LeFab70 <kouonang2002@gmail.com>
2026-03-15 22:10:44 -0300

    some validations

--- models/User.cs
+++ models/User.cs
@@ -1,12 +1,20 @@
 //creation des users qui loueront les livres de la bibliothèque
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiProjetBorrowing.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
